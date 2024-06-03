164ae388f9a6d9ef8467ed048137737efc177ebf
Garbi93 <iseung2093@gmail.com>
2024-06-03 18:24:05 +0900

    06/03 security config BCrypt 암호화 셋팅 및 properties 설정

--- src/main/java/com/example/testsecurity20240603/config/SecurityConfig.java
+++ src/main/java/com/example/testsecurity20240603/config/SecurityConfig.java
@@ -4,13 +4,22 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    // 비크립트 암호화 메서드 
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // 인증, 인가 작업
         http
                 .authorizeHttpRequests((auth)-> auth
                         .requestMatchers("/","/login").permitAll()
@@ -19,12 +28,14 @@ public class SecurityConfig {
                         .anyRequest().authenticated()
                 );
 
+        // 로그인 관련 작업
         http
                 .formLogin((auth) -> auth.loginPage("/login")
                         .loginProcessingUrl("/loginProc")
                         .permitAll()
                 );
 
+        // csrf 필터 관련 작업
         http
                 .csrf((auth) -> auth.disable());
 
