96420bf560a3555957f3ca22fd8f81f347366083
Garbi93 <iseung2093@gmail.com>
2024-06-03 15:25:09 +0900

    06/03 first-commit

---
+++ src/main/java/com/example/testsecurity20240603/TestSecurity20240603Application.java
@@ -0,0 +1,13 @@
+package com.example.testsecurity20240603;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSecurity20240603Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestSecurity20240603Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testsecurity20240603/TestSecurity20240603ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testsecurity20240603;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSecurity20240603ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
